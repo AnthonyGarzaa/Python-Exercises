55d2e3385d4df9f4755ebbe67e4710e073969fd4
AnthonyGarzaa <47547459+AnthonyGarzaa@users.noreply.github.com>
2019-04-20 23:04:56 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,8 +6,26 @@ wn.bgcolor("indigo")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# Main Game Loop
+# Paddle A 
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+# Main Game Loop
 while True:
     wn.update()
 
