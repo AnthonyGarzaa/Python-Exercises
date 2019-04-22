f56ec6768e422af3bcc4e7fda3d0164439271dbd
AnthonyGarzaa <47547459+AnthonyGarzaa@users.noreply.github.com>
2019-04-22 07:42:34 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -24,6 +24,20 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
 
 # Main Game Loop
 while True:
